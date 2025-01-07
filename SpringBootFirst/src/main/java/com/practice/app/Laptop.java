5fdf20fe05fb5483dda20deb66bb9ba3bbca23d7
Srikar Kale Kshema <srikar.k@kshema.co>
2025-01-07 15:28:44 +0530

    Added all new files

---
+++ SpringBootFirst/src/main/java/com/practice/app/Laptop.java
@@ -0,0 +1,12 @@
+package com.practice.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	
+	public void compile() {
+		System.out.println("Compiling");
+	}
+
+}
