7754394eeb78a70e3a79ffec9e85aa97fafa5dea
Srikar Kale Kshema <srikar.k@kshema.co>
2025-01-15 17:12:33 +0530

    Saving to push to Github

---
+++ Spring1/src/main/java/org/practice/Spring1/Alien.java
@@ -0,0 +1,24 @@
+package org.practice.Spring1;
+
+public class Alien {
+	
+	int age;
+	
+	public Alien() {
+		System.out.println("Alien Object created");
+	}
+	
+	public void code() {
+		System.out.println("Coding");
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
---
+++ Spring1/src/main/java/org/practice/Spring1/Laptop.java
@@ -0,0 +1,8 @@
+package org.practice.Spring1;
+
+public class Laptop {
+
+	public Laptop() {
+		System.out.println("Laptop Object created");
+	}
+}
