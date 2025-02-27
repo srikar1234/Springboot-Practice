a3271d4a0b2695c936a113facf68adc7807859cb
Srikar Kale :: Kshema <srikar.k@kshema.co>
2025-02-27 20:33:04 +0530

    Java course

--- Spring1/src/main/java/org/practice/Spring1/App.java
+++ Spring1/src/main/java/org/practice/Spring1/App.java
@@ -7,13 +7,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
-    	System.out.println( "Hello World!" );
-    	
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+
     	//This below line creates a container for us
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         
@@ -25,15 +22,21 @@ public class App
 //        Alien obj = (Alien) context.getBean("alien");
 //        obj.code();
 
+// Scopes: Singleton, and Prototype
+//        Alien obj1 = (Alien) context.getBean("alien");
+//        System.out.println(obj1.getAge());
+//        obj1.code();
+//
+//        obj1.setAge(21);
+//
+//        Alien obj2 = (Alien) context.getBean("alien");
+//        System.out.println(obj2.getAge());
+//        obj2.code();
+
+//        Setter Injection
         Alien obj1 = (Alien) context.getBean("alien");
         System.out.println(obj1.getAge());
         obj1.code();
-        
         obj1.setAge(21);
-        
-        Alien obj2 = (Alien) context.getBean("alien");
-        System.out.println(obj2.getAge());
-        obj2.code();
-        
     }
 }
--- Spring1/src/main/java/org/practice/Spring1/Laptop.java
+++ Spring1/src/main/java/org/practice/Spring1/Laptop.java
@@ -1,8 +1,13 @@
 package org.practice.Spring1;
 
-public class Laptop {
+public class Laptop implements Computer {
 
 	public Laptop() {
 		System.out.println("Laptop Object created");
+	}
+
+	@Override
+	public void compile(){
+		System.out.println("Compiling using Laptop");
 	}
 }
