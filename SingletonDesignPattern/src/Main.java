4bd1b55b42c977b8a056c89d7caba446cbf971a5
Srikar Kale <srikar.kale01@gmail.com>
2025-11-12 23:09:02 +0530

    Singleton Pattern

--- SingletonDesignPattern/src/Main.java
+++ SingletonDesignPattern/src/Main.java
@@ -2,5 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Logger logger1 = Logger.getInstanceOfLogger();
+        logger1.log("This message is from logger1");
+        Logger logger2 = Logger.getInstanceOfLogger();
+        logger2.log("This message is from logger2 ");
     }
 }
