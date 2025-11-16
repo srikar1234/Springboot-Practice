717b17aa7d042e312d8fcb6606852a30e472bf76
Srikar Kale <srikar.kale01@gmail.com>
2025-11-16 22:08:58 +0530

    Factory Design pattern Implemented

--- SingletonDesignPattern/src/Logger.java
+++ SingletonDesignPattern/src/Logger.java
@@ -3,16 +3,20 @@ import java.util.Objects;
 public class Logger {
 
     static int counter = 0;
-    private static Logger loggerInstance = null;
+    private static volatile Logger loggerInstance = null;
     //Private constructor disallows user from creating a new object
     private Logger(){
         counter +=1;
         System.out.println("New Logger created. Counter number: " + counter);
     }
 
-    static Logger getInstanceOfLogger(){
-        if(Objects.isNull(loggerInstance))
-            loggerInstance = new Logger();
+    static  Logger getInstanceOfLogger(){
+        if(Objects.isNull(loggerInstance)){
+            synchronized (Logger.class){
+                if(Objects.isNull(loggerInstance))
+                    loggerInstance = new Logger();
+            }
+        }
         return loggerInstance;
     }
 
